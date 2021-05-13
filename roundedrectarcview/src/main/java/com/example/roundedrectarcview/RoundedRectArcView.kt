612246646e2bb32c141c11e20298a32e51f52779
Anwesh43 <anweshthecool0@gmail.com>
2021-05-14 04:42:04 +0530

    adding RoundedRectArcView class

--- roundedrectarcview/src/main/java/com/example/roundedrectarcview/RoundedRectArcView.kt
+++ roundedrectarcview/src/main/java/com/example/roundedrectarcview/RoundedRectArcView.kt
@@ -54,6 +54,21 @@ fun Canvas.drawRRANode(i : Int, scale : Float, paint : Paint) {
     drawRoundedRectArc(scale, w, h, paint)
 }
 
+class RoundedRectArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
 
 
 
