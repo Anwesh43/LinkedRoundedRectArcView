2268d366cfab46db9ff4549efe59c1481fe0e3cf
Anwesh43 <anweshthecool0@gmail.com>
2021-05-14 04:50:09 +0530

    adding RRANode class

--- roundedrectarcview/src/main/java/com/example/roundedrectarcview/RoundedRectArcView.kt
+++ roundedrectarcview/src/main/java/com/example/roundedrectarcview/RoundedRectArcView.kt
@@ -116,6 +116,47 @@ class RoundedRectArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RRANode(var i : Int, val state : State = State()) {
+
+        private var next : RRANode? = null
+        private var prev : RRANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RRANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRRANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RRANode {
+            var curr : RRANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
 
 
