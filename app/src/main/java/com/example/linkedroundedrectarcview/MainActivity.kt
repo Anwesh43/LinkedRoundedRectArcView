8db7d45306ebc2f8a66d2e1afcbb5848531baa60
Anwesh43 <anweshthecool0@gmail.com>
2021-05-14 05:16:36 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedroundedrectarcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedroundedrectarcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedroundedrectarcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.roundedrectarcview.RoundedRectArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RoundedRectArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
